28df69a1a37807fd7758d046b2ed09b03b793661
Francis Bawa <francis_bawa@dlsu.edu.ph>
2025-03-07 19:59:51 +0800

    added session

--- website/views.py
+++ website/views.py
@@ -1,29 +1,43 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, session
 from .utils import *
+from .backend import *
 
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
-    md = ""
-    input_string = ""
-    memory = {}
-    logic = {}
+    if 'md' not in session:
+        session['md'] = ""
+    if 'input_string' not in session:
+        session['input_string'] = ""
+    if 'is_started' not in session:
+        session['is_started'] = False
 
     if request.method == 'POST':
-        md = request.form.get('machine-definition')
-        input_string = request.form.get('input-string')
 
-        memory, logic, valid, error = extractMachineDefinition(md)
+        if 'run' in request.form:
+            memory = {}
+            logic = {}
+            session['md'] = request.form.get('machine-definition')
+            session['input_string'] = request.form.get('input-string')
 
-        if not valid:
-            flash(error, category='error')
-        else:
-            print(memory)
-            print(logic)
-            print("Machine definition is valid")
+            memory, logic, valid, error = extractMachineDefinition(session['md'])
+
+            if not valid:
+                flash(error, category='error')
+            else:
+                index, state, mem_contents = initializeAutomata(memory, logic, session['input_string'])
+                session['is_started'] = True
+                print(index)
+                print(state)
+                print(mem_contents)
+                print("Machine definition is valid")
     
-    return render_template("index.html", type="Step by State", md=md, input_string=input_string)
+        if 'reset' in request.form:
+            session.pop('is_started', None)
+            session['is_started'] = False
+
+    return render_template("index.html", type="Step by State", is_started=session['is_started'], md=session['md'], input_string=session['input_string'])
 
 @views.route('/multiple-run', methods=['GET', 'POST'])
 def multiple_run():
