ec621243749abfa8e2a2d363d45a990c1853e736
Francis Bawa <francis_bawa@dlsu.edu.ph>
2025-03-07 19:53:56 +0800

    configured secret key

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from website import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
+    app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY", "fallback_secret_key")
     app.run(debug=True)
