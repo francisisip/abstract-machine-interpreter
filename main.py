27ce4b7fdbfc881ad944eafa86b209c2d2f1d45f
Francis Bawa <francis_bawa@dlsu.edu.ph>
2025-03-11 20:08:28 +0800

    refactor

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY", "fallback_secret_key")
+    app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY", "emergency_secret_key")
     app.run(debug=True)
